39a47f6f9ecc20aebbe9f07c7f5efa6234e576dd
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-11-20 18:10:26 -0300

    Update ArmazenamentoHumorAlunos.java
    ok

--- src/armazenamento/ArmazenamentoHumorAlunos.java
+++ src/armazenamento/ArmazenamentoHumorAlunos.java
@@ -14,8 +14,8 @@ public class ArmazenamentoHumorAlunos implements Armazenamento{
 	private Path arquivo_path;
 	
 	public ArmazenamentoHumorAlunos() {
-		this.nomeArquivo = "/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy/Dados/HumorAlunos.txt";
-		this.arquivo_path = Paths.get("/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy/Dados/HumorAlunos.txt");
+		this.nomeArquivo = "C:/Users/Gabri/IdeaProjects/Be-Happy/Dados/HumorAlunos.txt";
+		this.arquivo_path = Paths.get("C:/Users/Gabri/IdeaProjects/Be-Happy/Dados/HumorAlunos.txt");
 	}
 	
 	public void escreverTexto(String texto) {
