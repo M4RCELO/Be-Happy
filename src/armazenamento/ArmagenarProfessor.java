4caaded7d24b9ceb8422fc1bb724ae07bfdcb855
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 14:46:07 -0300

    Create ArmagenarProfessor.java
    ok

---
+++ src/armazenamento/ArmagenarProfessor.java
@@ -0,0 +1,23 @@
+package armazenamento;
+
+import buscas.Login;
+import dominio.AlunoLogado;
+import dominio.ProfessorLogado;
+import outros.DataEHora;
+
+public class ArmagenarProfessor {
+    private ArmagenamentoHumorAlunosProfessor armagenamentoHumorAlunosProfessor = new ArmagenamentoHumorAlunosProfessor ();
+    private ArmagenamentoRelatosProfessor armagenamentoRelatosProfessor = new ArmagenamentoRelatosProfessor();
+    private ProfessorLogado professor = Login.professorLogado;
+    private AlunoLogado aluno = Login.alunoLogado;
+    private DataEHora dataEHora = new DataEHora();
+
+    public void humor(String humor) {
+        String infos = professor.getMatricula().toString()+", "+aluno.getMatricula().toString()+", "+humor+", "+dataEHora.getData()+", "+dataEHora.getHora()+" ;";
+        armagenamentoHumorAlunosProfessor.escreverTexto(infos);
+    }
+    public void relato(String relatoStr) {
+        String infos = professor.getMatricula().toString()+", "+aluno.getMatricula().toString()+", "+relatoStr+", "+dataEHora.getData()+", "+dataEHora.getHora()+" ;";
+        armagenamentoRelatosProfessor.escreverTexto(infos);
+    }
+}
