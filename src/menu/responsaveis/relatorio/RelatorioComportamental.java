ac284d193f6822023557f25be55477a15eed011b
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 14:47:37 -0300

    Create RelatorioComportamental.java
    ok

---
+++ src/menu/responsaveis/relatorio/RelatorioComportamental.java
@@ -0,0 +1,21 @@
+package menu.responsaveis.relatorio;
+
+import armazenamento.ArmazenamentoComportamentoDiario;
+import outros.ObterValores;
+
+public class RelatorioComportamental extends ObterValores {
+    ArmazenamentoComportamentoDiario comportamentoDiario = new ArmazenamentoComportamentoDiario();
+
+    public void RComportamental(String matriculaAluno){
+        String texto = comportamentoDiario.lerTexto();
+        String[] users = texto.split(";");
+        for (int i = 0; i < users.length; i++) {
+            String[] matriz_users = users[i].split(",");
+            matriz_users[1] = matriz_users[1].replace(" ", "");
+            if (matriz_users[3].equals(matriculaAluno)){
+                System.out.println(users[i]);
+            }
+        }
+    }
+
+}
