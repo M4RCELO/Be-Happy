9059c536b6e74b9a13a01604dc5afdf2bf685e7e
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 14:47:15 -0300

    Create AcontecimentoCoordenação.java
    ok

---
+++ src/menu/professor/Coordenacao/AcontecimentoCoordenação.java
@@ -0,0 +1,15 @@
+package menu.professor.Coordenacao;
+
+import armazenamento.ArmazenamentoAcontecimentosCoordenacao;
+import outros.ObterValores;
+
+public class AcontecimentoCoordenação extends ObterValores {
+    ArmazenamentoAcontecimentosCoordenacao acontecimentos = new ArmazenamentoAcontecimentosCoordenacao();
+
+    public void Acontecimento(String matricula){
+        String acontecimento = obterValorString("O que aconteceu?");
+        String noti= acontecimento +","+matricula;
+        acontecimentos.escreverTexto(noti);
+
+    }
+}
