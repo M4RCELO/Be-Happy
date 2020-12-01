b4aa61a93faa7189496060877e76324bc65e5c33
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 14:41:28 -0300

    Create ArmagenamentoHumorAlunosProfessor.java
    ok

---
+++ src/armazenamento/ArmagenamentoHumorAlunosProfessor.java
@@ -0,0 +1,48 @@
+package armazenamento;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ArmagenamentoHumorAlunosProfessor implements Armazenamento{
+    private String nomeArquivo;
+    private Path arquivo_path;
+
+    public ArmagenamentoHumorAlunosProfessor() {
+        this.nomeArquivo = "C:/Users/Gabri/IdeaProjects/Be-Happy/Dados/HumorAlunosProfessor.txt";;
+        this.arquivo_path = Paths.get(nomeArquivo);
+    }
+
+
+
+    public void escreverTexto(String texto) {
+        try {
+            FileWriter fw = new FileWriter(nomeArquivo, true);
+            BufferedWriter conexao = new BufferedWriter(fw);
+            conexao.write(texto);
+            conexao.newLine();
+            conexao.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String lerTexto() {
+        String texto = "";
+        try {
+            List<String> linhas = Files.readAllLines(arquivo_path);
+            for (String linha : linhas) {
+                texto += linha;
+                texto += " ";
+            }
+            texto = texto.trim();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return texto;
+    }
+}
