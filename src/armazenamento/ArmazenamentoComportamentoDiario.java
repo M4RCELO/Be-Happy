6b35c296dd69d6f84111460723df9edbc7e49cd0
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:09:45 -0300

    Update ArmazenamentoComportamentoDiario.java
    ok

--- src/armazenamento/ArmazenamentoComportamentoDiario.java
+++ src/armazenamento/ArmazenamentoComportamentoDiario.java
@@ -1,48 +1,13 @@
 package armazenamento;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
-public class ArmazenamentoComportamentoDiario implements Armazenamento{
-    private String nomeArquivo;
-    private Path arquivo_path;
+public class ArmazenamentoComportamentoDiario extends AmazenamentoAbstract{
 
     public ArmazenamentoComportamentoDiario() {
-        this.nomeArquivo = "/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy2/Dados/ComportamentoDiario.txt";
+        this.nomeArquivo = caminho +"ComportamentoDiario.txt";
         this.arquivo_path = Paths.get(nomeArquivo);
 
     }
 
-
-    public void escreverTexto(String texto) {
-        try {
-            FileWriter fw = new FileWriter(nomeArquivo, true);
-            BufferedWriter conexao = new BufferedWriter(fw);
-            conexao.write(texto);
-            conexao.newLine();
-            conexao.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String lerTexto() {
-        String texto = "";
-        try {
-            List<String> linhas = Files.readAllLines(arquivo_path);
-            for (String linha : linhas) {
-                texto += linha;
-                texto += " ";
-            }
-            texto = texto.trim();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return texto;
-    }
 }
