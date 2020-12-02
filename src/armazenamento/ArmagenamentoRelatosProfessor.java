aa3e9a7f3f469d416aa4a9e1ac3c19ba9a6aa7ef
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:09:35 -0300

    Update ArmagenamentoRelatosProfessor.java
    ok

--- src/armazenamento/ArmagenamentoRelatosProfessor.java
+++ src/armazenamento/ArmagenamentoRelatosProfessor.java
@@ -1,49 +1,12 @@
 package armazenamento;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
-public class ArmagenamentoRelatosProfessor implements Armazenamento{
-
-    private String nomeArquivo;
-    private Path arquivo_path;
+public class ArmagenamentoRelatosProfessor extends AmazenamentoAbstract{
 
     public ArmagenamentoRelatosProfessor() {
-        this.nomeArquivo = "/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy2/Dados/RelatosProfessor.txt";
+        this.nomeArquivo = caminho +"RelatosProfessor.txt";
         this.arquivo_path = Paths.get(nomeArquivo);
-    }
-    @Override
-    public void escreverTexto(String texto) {
-        try {
-            FileWriter fw = new FileWriter(nomeArquivo, true);
-            BufferedWriter conexao = new BufferedWriter(fw);
-            conexao.write(texto);
-            conexao.newLine();
-            conexao.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public String lerTexto() {
-        String texto = "";
-        try {
-            List<String> linhas = Files.readAllLines(arquivo_path);
-            for (String linha : linhas) {
-                texto += linha;
-                texto += " ";
-            }
-            texto = texto.trim();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return texto;
     }
 
 }
