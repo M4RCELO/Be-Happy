e35cf64fa3656877bac113a956f5ca4678f08f1e
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:09:57 -0300

    Update ArmazenamentoRelatos.java
    ok

--- src/armazenamento/ArmazenamentoRelatos.java
+++ src/armazenamento/ArmazenamentoRelatos.java
@@ -8,43 +8,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class ArmazenamentoRelatos implements Armazenamento{
-
-	private String nomeArquivo;
-    private Path arquivo_path;
+public class ArmazenamentoRelatos extends AmazenamentoAbstract{
 
     public ArmazenamentoRelatos() {
-        this.nomeArquivo = "/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy2/Dados/Relatos.txt";
+        this.nomeArquivo = caminho+"Relatos.txt";
         this.arquivo_path = Paths.get(nomeArquivo);
     }
-	
-	@Override
-	 public void escreverTexto(String texto) {
-        try {
-            FileWriter fw = new FileWriter(nomeArquivo, true);
-            BufferedWriter conexao = new BufferedWriter(fw);
-            conexao.write(texto);
-            conexao.newLine();
-            conexao.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-	@Override
-    public String lerTexto() {
-        String texto = "";
-        try {
-            List<String> linhas = Files.readAllLines(arquivo_path);
-            for (String linha : linhas) {
-                texto += linha;
-                texto += " ";
-            }
-            texto = texto.trim();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return texto;
-    }
-
 }
