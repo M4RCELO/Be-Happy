92de795ff721bf1107d4818b6423ca587710d0da
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:09:55 -0300

    Update ArmazenamentoNotificacao.java
    ok

--- src/armazenamento/ArmazenamentoNotificacao.java
+++ src/armazenamento/ArmazenamentoNotificacao.java
@@ -8,41 +8,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class ArmazenamentoNotificacao implements Armazenamento{
-    private String nomeArquivo;
-    private Path arquivo_path;
+public class ArmazenamentoNotificacao extends AmazenamentoAbstract{
 
     public ArmazenamentoNotificacao() {
-        this.nomeArquivo = "/home/marcelo/Área de Trabalho/Códigos/Java/Be-Happy2/Dados/Notificacao.txt";
+        this.nomeArquivo = caminho+"Notificacao.txt";
         this.arquivo_path = Paths.get(nomeArquivo);
-    }
-
-    @Override
-    public void escreverTexto(String texto) {
-        try {
-            FileWriter fw = new FileWriter(nomeArquivo, true);
-            BufferedWriter conexao = new BufferedWriter(fw);
-            conexao.write(texto);
-            conexao.newLine();
-            conexao.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public String lerTexto() {
-        String texto = "";
-        try {
-            List<String> linhas = Files.readAllLines(arquivo_path);
-            for (String linha : linhas) {
-                texto += linha;
-                texto += " ";
-            }
-            texto = texto.trim();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        return texto;
     }
 }
