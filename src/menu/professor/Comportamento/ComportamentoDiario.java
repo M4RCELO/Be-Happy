6f3c5d2ac12ab3dc13f41b3ea30ae72c9de446eb
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:44:23 -0300

    Update ComportamentoDiario.java
    ok

--- src/menu/professor/Comportamento/ComportamentoDiario.java
+++ src/menu/professor/Comportamento/ComportamentoDiario.java
@@ -1,13 +1,16 @@
 package menu.professor.Comportamento;
 
+import armazenamento.ArmagenamentoRelatosProfessor;
 import armazenamento.ArmazenamentoComportamentoDiario;
 import outros.ObterValores;
 
 public class ComportamentoDiario extends ObterValores {
    ArmazenamentoComportamentoDiario comportamentoDiario = new ArmazenamentoComportamentoDiario();
+   ArmagenamentoRelatosProfessor relatosProfessor = new ArmagenamentoRelatosProfessor();
     public void compotamentoDiarioAluno( String matricula){
         String comportamento = obterValorString("Qual foi o comportamento desse aluno no dia?");
         String noti= comportamento +","+matricula+";";
         comportamentoDiario.escreverTexto(noti);
+        relatosProfessor.escreverTexto(noti);
     }
 }
