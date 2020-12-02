be5a754c5933232557271a1f15a5a3dee80a4047
João Gabriel <44182255+Jotage777@users.noreply.github.com>
2020-12-01 23:44:28 -0300

    Update RelatorioProfessor.java
    ok

--- src/menu/professor/Relatorio/RelatorioProfessor.java
+++ src/menu/professor/Relatorio/RelatorioProfessor.java
@@ -1,27 +1,17 @@
 package menu.professor.Relatorio;
 
-import armazenamento.ArmazenamentoHumorAlunosProfessor;
 import armazenamento.ArmagenamentoRelatosProfessor;
 import outros.ObterValores;
 
 public class RelatorioProfessor extends ObterValores {
     ArmagenamentoRelatosProfessor armagenamentoRelatosProfessor = new ArmagenamentoRelatosProfessor();
-    ArmazenamentoHumorAlunosProfessor armagenamentoHumorAlunosProfessor = new ArmazenamentoHumorAlunosProfessor();
 
     public void RelatorioDosProfessores(int tipo){ // colocar para ser os relatos só da turma do professor
-        if(tipo==1){
             String texto = armagenamentoRelatosProfessor.lerTexto();
             String[] users = texto.split(";");
             for (int i = 0; i < users.length; i++) {
                 System.out.println(users[i]);
             }
-        }
-        else if(tipo==2){ // colocar para ser os relatos só da turma do professor
-            String texto = armagenamentoHumorAlunosProfessor.lerTexto();
-            String[] users = texto.split(";");
-            for (int i = 0; i < users.length; i++) {
-                System.out.println(users[i]);
-            }
-        }
+
     }
 }
